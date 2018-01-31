c517824d80036fcdea2dd63387c7b1bd365e36c7
Amber <ambersethi97@gmail.com>
2018-02-01 00:00:42 +0530

    second

--- LandMark Image Book/LandMark Image Book/ViewController.swift
+++ LandMark Image Book/LandMark Image Book/ViewController.swift
@@ -11,23 +11,38 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
+    var landmarkNamesArray = [String]()
+    var landmarkImageArray = [UIImage]()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+//tableview setup
         tableView.dataSource = self
         tableView.delegate = self
         
+//example array creation
+        landmarkNamesArray.append("Colosseum")
+        landmarkNamesArray.append("Great Wall")
+        landmarkNamesArray.append("Kremlin")
+        landmarkNamesArray.append("Stonehenge")
+        landmarkNamesArray.append("Taj Mahal")
         
+        landmarkImageArray.append(UIImage(named:"colosseum.jpg")!)
+        landmarkImageArray.append(UIImage(named:"great wall.jpg")!)
+        landmarkImageArray.append(UIImage(named:"kremlin.jpg")!)
+        landmarkImageArray.append(UIImage(named:"stonehenge.jpg")!)
+        landmarkImageArray.append(UIImage(named:"Taj mahal.jpg")!)
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return landmarkNamesArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "A"
+        cell.textLabel?.text = landmarkNamesArray[indexPath.row]
         return cell
     }
    
