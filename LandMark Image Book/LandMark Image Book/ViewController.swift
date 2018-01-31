17e9a659bdd3f1d1cf5f2b2431b06ff28700179e
Amber <ambersethi97@gmail.com>
2018-01-31 23:32:01 +0530

    first

---
+++ LandMark Image Book/LandMark Image Book/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  LandMark Image Book
+//
+//  Created by Amber Sethi on 29/01/18.
+//  Copyright © 2018 Amber Sethi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "A"
+        return cell
+    }
+   
+}
+
